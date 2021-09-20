62a3a06842775c08f10ca367887b9976d99e8fab
Syed-sahil <66295936+Syed-sahil@users.noreply.github.com>
2021-09-21 01:03:12 +0530

    Add files via upload

---
+++ python assignment day 1.py
@@ -0,0 +1,5 @@
+p=float(input("Enter principle amount value:"))
+t=float(input("Enter time period value(years):"))
+r=float(input("Enter rate value:"))
+si = (p * t * r)/100
+print('The simple interest is', si)
